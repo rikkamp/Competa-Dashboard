1a4300ec26a5a41c4b52d154a7b4efad9aa8c23c
lvandernoll <lvandernoll@gmail.com>
2018-09-07 04:26:32 -0400

    made the text inside the mp4 items more readable

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 window.onload = () => {
 	document.querySelector('#autofocus').focus();
-	const timelineItems = document.querySelectorAll(".timeline__item--mp4");
-	timelineItems.forEach( item => {
+
+	const bigMp4Items = document.querySelectorAll(".timeline__item--mp4-big");
+	bigMp4Items.forEach( item => {
 		item.style.backgroundImage = `url(${item.attributes['data-image-src'].value})`;
+	});
+
+	const smallMp4Items = document.querySelectorAll(".timeline__item--mp4-small");
+	smallMp4Items.forEach( item => {
+		item.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, .05) 70%, rgba(0, 0, 0, .6) 100%), url(${item.attributes['data-image-src'].value})`;
 	});
 };
